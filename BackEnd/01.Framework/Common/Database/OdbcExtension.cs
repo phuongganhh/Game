ce8c6491a6a6da20b4bec4d87607b56ff69e9f78
nhduy <nhduy@NHDUY>
2019-10-24 18:01:56 +0700

    db

--- BackEnd/01.Framework/Common/Database/OdbcExtension.cs
+++ BackEnd/01.Framework/Common/Database/OdbcExtension.cs
@@ -33,6 +33,32 @@ namespace Common.Database
         {
             var sql = query.Complie();
 
+            using(var cmd = DatabaseConnectService.Instance.Connection.CreateCommand())
+            {
+                cmd.CommandText = sql.RawSql;
+                cmd.Parameters.Map(sql.NamedBindings);
+                using(var reader = await cmd.ExecuteReaderAsync())
+                {
+                    var lst = new List<T>();
+                    while (await reader.ReadAsync())
+                    {
+                        var dic = new Dictionary<string, object>();
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            dic[reader.GetName(i)] = reader.GetValue(i);
+                        }
+                        var json = JsonConvert.SerializeObject(dic);
+                        lst.Add(JsonConvert.DeserializeObject<T>(json));
+                    }
+                    return lst;
+                }
+
+            }
+        } 
+        public static async Task<IEnumerable<T>> Fetch<T>(this Query query)
+        {
+            var sql = query.Complie();
+
             var values = new Dictionary<string, string>();
             values["user"] = "test";
             values["db"] = "pa";
